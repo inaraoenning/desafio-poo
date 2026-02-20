59bacd9a85a074a05d8849dda1ea261c49d5ae57
inara <inaraoenning@gmail.com>
2026-02-20 12:48:45 -0300

    Add desafio3

--- src/desafio2.java
+++ src/desafio2.java
@@ -1,2 +1,6 @@
 public class desafio2 {
+
+//    Armazene o nome em uma variável, em seguida mostre a seguinte mensagem:
+//            “Bem vindo Fulaninho!”
+
 }
---
+++ src/desafio3.java
@@ -0,0 +1,11 @@
+public class desafio3 {
+    public static void main(String[] args) {
+
+//        Quando o usuário selecionar compra, o programa deve armazenar o nome do produto e seu preço, além de descontar do Financeiro um o preço do produto.
+//                Quando o usuário selecionar venda, o programa deve retirar um (ou mais) produto(s) de acordo com a solicitação do usuário. Ao fazer isso deve-se adicionar o valor desse mesmo produto(s) no Financeiro, porém adicione 5% a mais para cada produto vendido.
+//                Por as opções Estoque e Financeiro servem para verificar quais produtos estão estocados e qual o valor em caixa, respectivamente.
+//
+//        Importante, sempre comece com R$100 em caixa.
+
+    }
+}
